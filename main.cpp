d9262b81446660e4f4d170abfc839008ed58e656
Joshua CHOI Kui Wang <94032950+joshuaSYSS@users.noreply.github.com>
2026-01-15 18:28:48 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include "core/board.h"
+#include "core/reversi.h"
+#include "mcts/node.h"
+#include "mcts/puct.h"
+#include "mcts/temperature.h"
 using namespace std;
 int main(void){
   return 0;
