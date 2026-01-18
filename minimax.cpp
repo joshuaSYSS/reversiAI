15aa381a40a9687d2b23e130d82800ef396755b4
Metamorphosis1116 <harrychoihoming@gmail.com>
2026-01-18 20:50:38 +0800

    Clean up minimax.cpp by removing unused code
    Removed unused macros, direction vector, and functions related to valid moves.

--- minimax.cpp
+++ minimax.cpp
@@ -1,37 +1,6 @@
 #include <vector>
 #include <set>
 using namespace std;
-#define a first
-#define b second
-
-vector<vector<int>> directions= {{1, -1}, {1, 0}, {1, 1}, 
-	                            {0, -1}         , {0, 1}, 
-	                            {-1, -1}, {-1, 0}, {-1, 1}};
-
-bool isavailable(vector<vector<int>>& board, int player, int i, int j,  direction){
-    //if not in range and its not == to opponent then  NULL
-    if(i>=0 && i<8 && j>=0 && j<8){
-        if(board[i][j] == -player)
-            return isavailable(board, player, i+direction.at(0), j+direction.at(1), direction);
-        else if(board[i][j] == 0)
-            return {i, j};
-    }
-    
-    return NULL;
-}
-
-vector<pair<int,int>> getvalidmove(vector<vector<int>>& board, int player){
-    vector<pair<int, int>> validmove;
-    
-    for(int block:board){
-        if(block==player){
-            for(auto d : directions){
-                if(isavailable(board, player, i, j, direction))
-                validmove.push_back({i, j});
-            }
-        }
-    }
-}
 
 //      i, j, score
 tuple<int, int, int> minimax(vector<vector<int>>& board, int depth, int a, int b, int player){   //player: 1/-1
