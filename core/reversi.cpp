1cf6123611ed32092fe609777571642818b57c98
Metamorphosis1116 <harrychoihoming@gmail.com>
2026-01-18 16:55:28 +0800

    Refactor direction handling and reverse logic

--- core/reversi.cpp
+++ core/reversi.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int BOARD_SIZE = 8;
 
 const int DIRECTION_SIZE = 8;
-const vector<vector<int>> DIRECTIONS = {{1, -1}, {1, 0}, {1, 1}, 
+const vector<pair<int, int>> DIRECTIONS = {{1, -1}, {1, 0}, {1, 1}, 
 	                            {0, -1}         , {0, 1}, 
 	                            {-1, -1}, {-1, 0}, {-1, 1}};
 
@@ -16,7 +16,7 @@ vector<vector<int>> board(BOARD_SIZE, vector<int>(BOARD_SIZE, 0));
 void place(int i, int j, int player){
     board[i][j] = player;
     
-    vector<bool> b = isavailable(board, player, i, j);
+    vector<bool> b = reversible_direaction(board, player, i, j);
     for(int d = 0; d < DIRECTION_SIZE; d++){
         if(b[d]){
             int x = i + DIRECTIONS[d][0];
@@ -29,6 +29,43 @@ void place(int i, int j, int player){
         }
     }
 }
+/*
+vector<bool> reversible_direaction(vector<vector<int>>& board, int player, int i, int j) {
+	vector<bool> b;
+	for(auto d : directions){
+		do{	int x=i+direction.first; y=j+direction.second;
+		  }while(x>=0 && y<8 && x>=0 && y<8 && board[x][y] == -player);
+		if(board[x][y] == player)	b.pushback(1);
+		else	b.pushback(0);
+	}	
+}
+*/
+
+//version 2 ->
+
+void place(int i, int j, int player){
+	for(auto d: DIRECTIONS){			//check for each direction, reverse all oponent chess between the two self chesses "o x x o"
+		reverse(board, player, i, j, d);
+	}
+		
+}
+
+bool reverse(vector<vector<int>>& board, int player, int i, int j, pair<int, int> direction){
+	bool doit = 0;
+	if(i+direction.first>=0 && i+direction.first<8 && j+direction.second>=0 && j+direction.second<8){
+		if(board[i+direction.first][j+direction.second] == 0){
+			return 0;
+		}else if(board[i+direction.first][j+direction.second] == -player){
+			doit = reverse(board, player, i+direction.first, j+direction.second, direction);
+		}else{
+			return 1;
+		}
+	}
+	if(doit){
+		board[i][j] == player;
+	}
+	return 1;
+}
 
 vector<bool> isavailable(vector<vector<int>>& board, int player, int i, int j) {
 	//if not in range and its not == to opponent then  NULL
@@ -40,6 +77,19 @@ vector<bool> isavailable(vector<vector<int>>& board, int player, int i, int j) {
 	}
 
 	return false;
+}
+
+vector<pair<int,int>> getvalidmove(vector<vector<int>>& board, int player){
+    vector<pair<int, int>> validmove;
+    
+    for(int block:board){
+        if(block==player){
+            for(auto d : directions){
+                if(isavailable(board, player, i, j, direction))
+                validmove.push_back({i, j});
+            }
+        }
+    }
 }
 
 vector<vector<int>> getBoard(){
