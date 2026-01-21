0175dfd472415301e58fb016ea9288480bc7f131
Metamorphosis1116 <harrychoihoming@gmail.com>
2026-01-21 20:28:04 +0800

    Update reversi.h

--- core/reversi.h
+++ core/reversi.h
@@ -1,9 +1,52 @@
+#ifndef REVERSI_H
+#define REVERSI_H
 #include <vector>
 #include <set>
+#include <optional>
+#include <iostream>
+#include <stack>
+
 using namespace std;
 
-set<pair<int, int>> getvalidmove(int);
-void place(int, int, int);
-vector<vector<int>> getBoard();
-int hasWinner(int);
-void printBoard(int);
+class Board{
+public:
+	const int BOARD_SIZE = 8;
+	const int DIRECTION_SIZE = 8;
+	const vector<pair<int, int>> DIRECTIONS =	{{1, -1},	{1, 0},		{1, 1},
+												{0, -1},				{0, 1},
+												{-1, -1},	{-1, 0},	{-1, 1}};
+
+	vector<vector<int>> board;
+
+	struct pers{
+		int i, j;
+		int prev_value;
+	};
+	stack<vector<pers>> persistent;
+	vector<pers> p;
+
+	void refreshBoard();
+
+	Board();
+
+	vector<vector<int>> getBoard() ;
+
+	void printBoard(int showplayermove=0) const;
+
+	void place(int i, int j, int player) ;
+
+	bool reverse(int player, int i, int j, pair<int, int> direction);
+
+	void undo();
+
+	optional<pair<int, int>> availablefield(const vector<vector<int>>& board, int player, int i, int j, pair<int, int> direction, bool op_found=0) const;
+
+	set<pair<int, int>> getvalidmove(int player) const;
+
+	int determineWinner();
+
+	int hasWinner(int player);
+	
+};
+
+#endif
