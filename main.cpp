f4509b445dbd2ac94483eb8432df157799ebe394
Metamorphosis1116 <harrychoihoming@gmail.com>
2026-01-21 20:27:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,28 +4,37 @@
 using namespace std;
 int main(void){
     int player = 1;
+    Board gameBoard = Board();
+    gameBoard.refreshBoard();
     while(true){
-        pair<int, int> move;
-        printBoard(player);
+        pair<int, int> move = {-1,-1};
+        gameBoard.printBoard(player);
 
         if(player == -1){
-            cout << "Your turn (format: row col): ";
             int i, j;
+            cout << "Your turn (format: row col): ";
             cin >> i >> j;
-            set<pair<int, int>> validmove = getvalidmove(player);
-            while(!validmove.count({i, j})){
+
+            if(i==-1  && j==-1){    //improve later: undo multiple times
+                gameBoard.undo();
+                gameBoard.undo();
+                gameBoard.printBoard(player);
+                cout << "undo done" << endl;
+                cout << "Your turn (format: row col): ";
+                cin >> i >> j;
+            }
+
+            while(!gameBoard.getvalidmove(player).count({i, j})){
                 cout << "Invalid move. Try again: ";
                 cin >> i >> j;
             }
             move = {i, j};
         }
         else{
-            cout << "AI is thinking..." << endl;
-            move = callAI(player);
-            cout << "AI plays: " << move.first << " " << move.second << endl;
+            move = callAI(gameBoard, player);
         }
-        place(move.first, move.second, player);
-        int nextplayer = hasWinner(player);
+        gameBoard.place(move.first, move.second, player);
+        int nextplayer = gameBoard.hasWinner(player);
 
         switch(nextplayer){
         	case 2:		cout << "Player 1 wins!" << endl; 	break;
