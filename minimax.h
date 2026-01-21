6216cb100795f88e112e620fa79e2c9563037570
Metamorphosis1116 <harrychoihoming@gmail.com>
2026-01-21 20:27:42 +0800

    Update minimax.h

--- minimax.h
+++ minimax.h
@@ -1,6 +1,10 @@
 #include <vector>
+#include <set>
+#include <stack>
+#include "weight.h"
+#include "core/reversi.h"
 using namespace std;
 
-int minimax(int, int, int, int);
+pair<int, int> callAI(const Board& gameBoard, int player);
 
-pair<int, int> callAI(int);
+int minimax(Board& virtualBoard, int depth, int a, int b, int player, int isMax, int rootPlayer);
